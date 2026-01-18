d148e2cc4cec42a6a4e69d413c6891e407ba9f8d
jplatt001 <jplatt001@citymail.cuny.edu>
2026-01-18 15:27:19 -0500

    Railway hosting

--- website/__init__.py
+++ website/__init__.py
@@ -1,39 +1,38 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from os import path
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    
-    db.init_app(app) 
 
-    from .models import User
+    # Environment variables (REQUIRED for cloud)
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev-secret")
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+        "DATABASE_URL", "sqlite:///local.db"
+    )
+
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
 
     from .routes import routes
-    app.register_blueprint(routes, url_prefix='/')
+    app.register_blueprint(routes)
 
     from .models import User
-    create_database(app)
 
     login_manager = LoginManager()
-    login_manager.login_view = 'routes.login'
+    login_manager.login_view = "routes.home"
     login_manager.init_app(app)
 
     @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    with app.app_context():
+        db.create_all()
 
     return app
-
-def create_database(app):
-    if not path.exists('website/'+ DB_NAME):
-        with app.app_context():
-            db.create_all()
-        print("Created Database!")
-

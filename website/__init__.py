78a3cbf38ac61e126fcf71c9bedff7709356fee1
jplatt001 <jplatt001@citymail.cuny.edu>
2026-02-24 18:57:38 -0500

    Fix Render postgres:// issue

--- website/__init__.py
+++ website/__init__.py
@@ -8,13 +8,15 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    # Environment variables (REQUIRED for cloud)
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev-secret")
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
-        "DATABASE_URL", "sqlite:///local.db"
-    )
+    database_url = os.getenv("DATABASE_URL", "sqlite:///local.db")
 
+    # Fix Render postgres:// issue
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
